d1fafc0bdd89da67af5f3b8f4e3f120254dcbd05
ValeMontecinosP <valemontecinosp@gmail.com>
2022-01-27 11:49:12 -0300

    js coso

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import data from './data/pokemon/pokemon.js';
 
 console.log(data.pokemon[1].name);
 
+const nuevop = document.createElement("section");
+const nuevoptexto = document.createTextNode("hola pos olvidona");
+nuevop.appendChild(nuevoptexto);
+document.main.appendChild(nuevop);
+//document.getElementById("nombrePk").appendChild(nuevop);
+
 let nombresPokemon;
 for (let i = 0; i < 251; ++i) {
     nombresPokemon = data.pokemon[i].name; 
     console.log(nombresPokemon);
-    document.getElementById("nombrePk").innerText=nombresPokemon;
+    document.getElementById("nombrePk").innerText+=nombresPokemon;
     } 
    
 //createElement
