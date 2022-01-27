e9329b120b88986f78a69bc5d9a4ff5d08521d26
pazvarela <89282860+pazvarela@users.noreply.github.com>
2022-01-27 12:53:18 -0400

    se trajo la data al html y se hicieron cambios en el css

--- src/main.js
+++ src/main.js
@@ -7,22 +7,32 @@ import data from './data/pokemon/pokemon.js';
 
 console.log(data.pokemon[1].name);
 
-const nuevop = document.createElement("section");
-const nuevoptexto = document.createTextNode("hola pos olvidona");
-nuevop.appendChild(nuevoptexto);
-document.main.appendChild(nuevop);
-//document.getElementById("nombrePk").appendChild(nuevop);
 
-let nombresPokemon;
-for (let i = 0; i < 251; ++i) {
-    nombresPokemon = data.pokemon[i].name; 
-    console.log(nombresPokemon);
-    document.getElementById("nombrePk").innerText+=nombresPokemon;
-    } 
-   
-//createElement
-//.appendChild
-//https://www.javascripttutorial.net/javascript-dom/javascript-createelement/
+let pokemon = data.pokemon;
+
+let cardbox = document.getElementById("cardBox")
+
+for ( let i = 0; i < pokemon.length; i++) {
+
+    let card = document.createElement("div");
+    let photo = document.createElement("img");
+    let name = document.createElement("p");
+
+
+    card.setAttribute("class", "box");
+    card.setAttribute("id", "cardId");
+    photo.setAttribute("src", pokemon[i].img);
+    photo.setAttribute("class", "image");
+    name.innerHTML = pokemon[i].name;
+
+    cardbox.appendChild(card);
+    card.appendChild(photo);
+    card.appendChild(name);
+
+
+
+
+}
 
 
 
