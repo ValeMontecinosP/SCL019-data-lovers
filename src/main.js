af7ee96d9326191ebb4ab58bebb17c5b4929ac60
ValeMontecinosP <valemontecinosp@gmail.com>
2022-02-01 10:48:09 -0300

    datos pokemon

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import data from './data/pokemon/pokemon.js';
 
 
 
-console.log(data.pokemon[1].name);
+console.log(data.pokemon[1]);
 
 
 let pokemon = data.pokemon;
+
+
 
 let cardbox = document.getElementById("cardBox")
 
@@ -29,23 +31,33 @@ for ( let i = 0; i < pokemon.length; i++) {
     photo.setAttribute("class", "image");
     card.appendChild(photo);
 
-    let name = document.createElement("p");
-    name.innerHTML = pokemon[i].name;
-    card.appendChild(name);
+    let namePkm = document.createElement("p");
+    namePkm.setAttribute("class", "name");
+    namePkm.innerHTML = pokemon[i].name;
+    card.appendChild(namePkm);
+
+    let aboutPkm = document.createElement("p");
+    aboutPkm.setAttribute("class", "about");
+    aboutPkm.innerHTML = pokemon[i].about;
+    card.appendChild(aboutPkm);
+
+    let typePkm = document.createElement("p");
+    typePkm.innerHTML = "Type: " + pokemon[i].type.join(", ");
+    card.appendChild(typePkm);
 
     let resistance = document.createElement("p");
     resistance.setAttribute("class","resistant");
-    resistance.innerHTML = pokemon[i].resistant;
+    resistance.innerHTML = "Resistance: " + pokemon[i].resistant.join(", ");
     card.appendChild(resistance);
 
     let weakness = document.createElement("p");
     weakness.setAttribute("class", "weak");
-    weakness.innerHTML = pokemon[i].weaknesses;
+    weakness.innerHTML = "Weaknesses: " + pokemon[i].weaknesses.join(", ");
     card.appendChild(weakness);
 
     let eggs = document.createElement("p");
     eggs.setAttribute("class","pkmegg");
-    eggs.innerHTML = pokemon[i].egg;
+    eggs.innerHTML = "Eggs: " + pokemon[i].egg;
     card.appendChild(eggs);
 
 
