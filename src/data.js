f8047978a2c62aee1b8e06e49b6d2278acfaeee4
ValeMontecinosP <valemontecinosp@gmail.com>
2022-02-03 16:59:21 -0300

    filter for each

--- src/data.js
+++ src/data.js
@@ -7,3 +7,8 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+export const typeFilter = (data, valor) => {
+  const filterTypeArr = data.filter(items => (items.generation[1] === valor || valor === 'All')); //propiedades del item 
+  return filterTypeArr;
+};
